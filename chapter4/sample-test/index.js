a416482fc9f57df924fde7105fee924b20b03417
zakumann <samuel742able@gmail.com>
2023-10-22 21:51:32 +0900

    chapter4

---
+++ chapter4/sample-test/index.js
@@ -0,0 +1,8 @@
+const calc = require("sample-package");
+const a = 17;
+const b = 3;
+
+console.log("a + b = ", calc.add(a,b));
+console.log("a - b = ", calc.sub(a,b));
+console.log("a * b = ", calc.multi(a,b));
+console.log("a / b = ", calc.div(a,b));
