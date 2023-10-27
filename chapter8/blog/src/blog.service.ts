0915b62f0732c3dca8abfd86b9d96bd56f891cc1
zakumann <samuel742able@gmail.com>
2023-10-28 01:28:42 +0900

    hello-nestjs

---
+++ chapter8/blog/src/blog.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PostDto } from './blog.model';
+import { BlogFileRepository, BlogRepository } from './blog.repository';
+
+@Injectable()
+export class BlogService {
+
+    constructor(private blogRepository: BlogFileRepository){}
+
+    async getAllPosts(){
+        return await this.blogRepository.getAllPost();
+    }
+
+    createPost(postDto: PostDto){
+        this.blogRepository.createPost(postDto);
+    }
+
+    async getPost(id): Promise<PostDto>{
+        return await this.blogRepository.getPost(id);
+    }
+
+    delete(id){
+        this.blogRepository.deletePost(id);
+    }
+
+    updatePost(id, postDto: PostDto){
+        this.blogRepository.updatePost(id, postDto);
+    }
+}
